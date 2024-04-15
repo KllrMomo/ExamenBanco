880de4f4bed54f1d5e550f8c441b86812420dbc3
KllrMomo <163612171+KllrMomo@users.noreply.github.com>
2024-04-15 07:51:51 -0700

    Add files via upload

---
+++ Transaccion.java
@@ -0,0 +1,34 @@
+
+package bake.examenbanco;
+
+public abstract class Transaccion {
+    protected String numeroIdentidadCliente;
+    protected double monto;
+
+    // Constructor
+    public Transaccion(String numeroIdentidadCliente, double monto) {
+        this.numeroIdentidadCliente = numeroIdentidadCliente;
+        this.monto = monto;
+    }
+    
+    //Metodo 
+    public abstract String obtenerInformacion();
+    
+    // Getters
+    public String getNumeroIdentidadCliente() {
+        return numeroIdentidadCliente;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    // Setters
+    public void setNumeroIdentidadCliente(String numeroIdentidadCliente) {
+        this.numeroIdentidadCliente = numeroIdentidadCliente;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+}
